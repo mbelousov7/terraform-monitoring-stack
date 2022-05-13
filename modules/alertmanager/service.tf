b9275f5e2cc6bca6fb4959bdf10a5cad5210dc1d
Mikhail Belousov <mikhail_belousov@epam.com>
2022-05-13 14:52:15 +0200

    alertmanager

--- modules/alertmanager/service.tf
+++ modules/alertmanager/service.tf
@@ -14,11 +14,31 @@ resource "kubernetes_service" "service" {
       protocol    = "TCP"
       target_port = var.container_port
     }
+  }
+}
+
+resource "kubernetes_service" "service-headless" {
+  metadata {
+    name      = "${var.name}-headless"
+    namespace = var.namespace
+    labels    = local.labels
+  }
+  spec {
+    selector         = local.labels
+    session_affinity = var.session_affinity
+    type             = var.service_type
+    cluster_ip       = "None"
 
     port {
       name        = "cluster"
       port        = var.cluster_port
       protocol    = "TCP"
+      target_port = var.cluster_port
+    }
+    port {
+      name        = "cluster-udp"
+      port        = var.cluster_port
+      protocol    = "UDP"
       target_port = var.cluster_port
     }
 
